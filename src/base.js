6d4c6295958ad5acf94786b447a61ce0251559d5
sergeyt <stodyshev@gmail.com>
2014-01-23 21:04:25 +0700

    skeleton

---
+++ src/base.js
@@ -0,0 +1,7 @@
+d3.chart = {
+	create: function(svg, def){
+		// TODO create axes
+		// TODO renderer based on chart type (should be easy extensible)
+		// TODO legend, could be a separate HTML widget
+	}
+};
