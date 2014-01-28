1155a758300513a292e8cc1f91bbcfafea96a04e
sergeyt <stodyshev@gmail.com>
2014-01-28 20:38:54 +0700

    improved demo

---
+++ demo/demo.js
@@ -0,0 +1,92 @@
+$(function(){
+	var column_chart = {
+		type: "column",
+		axes: {
+			x: {majorTicks: "outside"},
+			y: {
+				majorTicks: "outside",
+				scalar: true,
+				margin: true,
+				grid: { major: true, minor: false }
+			}
+		},
+		categories: ["A", "B", "A"],
+		series: ["Revenue", "Units"],
+		data: [
+			[90, 70, 95],
+			[30, 25, 27]
+		],
+		"title": {
+			"text": "Test Chart",
+			"position": "center"
+		},
+		"legend": {
+			"position": "topcenter"
+		}
+	};
+
+	var line_chart = {
+		type: "line",
+		axes: {
+			x: { majorTicks: "outside" },
+			y: {
+				majorTicks: "outside",
+				scalar: true,
+				margin: true,
+				grid: { major: true, minor: false }
+			}
+		},
+		categories: ["A", "B", "A"],
+		series: ["Revenue", "Units"],
+		data: [
+			[90, 70, 95],
+			[55, 80, 67]
+		],
+		title: {
+			text: "Test Chart",
+			position: "center"
+		},
+		legend: {
+			position: "topcenter"
+		}
+	};
+
+	var area_chart = {
+		type: "area",
+		axes: {
+			x: { majorTicks: "outside" },
+			y: {
+				majorTicks: "outside",
+				scalar: true,
+				margin: true,
+				grid: { major: true, minor: false }
+			}
+		},
+		categories: ["A", "B", "A"],
+		series: ["Revenue", "Units"],
+		data: [
+			[90, 70, 95],
+			[55, 80, 67]
+		],
+		title: {
+			text: "Test Chart",
+			position: "center"
+		},
+		legend: {
+			position: "topcenter"
+		}
+	};
+
+	var charts = window.__charts__ = {
+		'column': column_chart,
+		'line': line_chart,
+		'area': area_chart
+	};
+
+	Object.keys(charts).forEach(function(key){
+		var template = '<div class="chart outlined" data-chart="{0}"></div>';
+		$(f3.format(template, key)).appendTo($('body'));
+	});
+
+	$('.chart').chart();
+});
