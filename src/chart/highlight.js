510d3a6cc383a98747ef53c477526461fac8e68d
sergeyt <stodyshev@gmail.com>
2014-01-28 19:23:01 +0700

    added transition

--- src/chart/highlight.js
+++ src/chart/highlight.js
@@ -2,11 +2,15 @@ d3.behavior.hightlight = function(){
 	d3.select(this)
 		.style('opacity', 0.75)
 		.on('mouseover.highlight', function(){
-			// TODO with transition?
-			d3.select(this).style('opacity', 0.95);
+			d3.select(this)
+				.transition()
+				.duration(100)
+				.style('opacity', 0.95);
 		})
 		.on('mouseout.highlight', function(){
-			// TODO with transition?
-			d3.select(this).style('opacity', 0.75);
+			d3.select(this)
+				.transition()
+				.duration(100)
+				.style('opacity', 0.75);
 		});
 };
