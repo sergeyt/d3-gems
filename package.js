3f641ab309b8c0090901eb830fb8b167701b3c1d
sergeyt <stodyshev@gmail.com>
2014-09-23 21:55:17 +0700

    meteor packaging

---
+++ package.js
@@ -0,0 +1,15 @@
+Package.describe({
+  name: "sergeyt:d3-gems",
+  summary: "d3-gems - collection of reusable widgets powered by d3.js.",
+  git: "https://github.com/sergeyt/d3-gems",
+  version: "0.0.8"
+});
+
+Package.onUse(function(api){
+  var client = ["client"];
+  api.use(['jquery', 'd3'], client);
+  api.addFiles('lib/d3-tip/index.js', client);
+  api.addFiles('dist/d3.gems.js', client);
+  api.addFiles('dist/d3.gems.css', client);
+});
+
